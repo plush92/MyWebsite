eb4d5aaf72e48591f8e3cb4c096876d21a3474a8
plush92 <138998413+plush92@users.noreply.github.com>
2025-06-09 18:36:00 -0400

    update home page with formatted text

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,21 +1,37 @@
 import CustomContainer from "../components/materialui/CustomContainer";
-import { Typography } from "@mui/material";
+import { Typography, Box, Divider } from "@mui/material";
+import introText from "../components/home/introText";
+import aboutText from "../components/home/aboutText";
+import featuredWorkText from "../components/home/featuredWorkText";
 
 const Home: React.FC = () => {
-    return (
-        <CustomContainer>
-            <Typography variant="h5" fontWeight="bold" mb={6}>
-       Header/Introduction
-            </Typography>
-            <Typography variant="h5" fontWeight="bold" mb={6}>
-       Featured Work
-            </Typography>
-            <Typography variant="h5" fontWeight="bold" mb={6}>
-       About
-            </Typography>
+  return (
+    <CustomContainer >
+      <Box mb={6} mt={6}>
+        <Typography variant="h4" fontWeight="bold" gutterBottom>
+          Who Am I?
+        </Typography>
+        {introText}
+      </Box>
+      <Divider sx={{ mb: 6 }} />
+      <Box mb={6}>
+        <Typography variant="h4" fontWeight="bold" gutterBottom>
+          Featured Work
+        </Typography>
+        {featuredWorkText}
+      </Box>
+      <Divider sx={{ mb: 6 }} />
+      <Box mb={6}>
+        <Typography variant="h4" fontWeight="bold" gutterBottom>
+          About
+        </Typography>
+        {aboutText}
+      </Box>
+    </CustomContainer>
+  );
+};
 
-      </CustomContainer>
-    );
-  }
+export default Home;
   
-  export default Home;
+//Make links to featured work (github, present screenshots)
+//Format text in a more readable format
