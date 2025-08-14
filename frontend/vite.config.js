008292b0530eb3cadb1de40fd955c787a24378df
plush92 <138998413+plush92@users.noreply.github.com>
2025-08-14 18:03:46 -0400

    update vite, index.html

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 
 export default defineConfig({
+  base: '/MyWebsite/',
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist', 
+    emptyOutDir: true
+  }
+});
