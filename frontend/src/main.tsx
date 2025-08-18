f5caa76150f95c0c4f93c8867e5266fbdbfec79c
plush92 <138998413+plush92@users.noreply.github.com>
2025-08-17 21:20:27 -0400

    change configurations

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,25 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom";
-import App from "./App";  // Importing App from App.tsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
+import App from './App';
 
-// Ensure rootElement is correctly typed, as it could be null
-// const rootElement = document.getElementById("root");
-// if (!rootElement) {
-//   throw new Error("Root element not found");
-// }
-
-// // Create the root and render the app
-// const root = ReactDOM.createRoot(rootElement);
-
-// // Render the app within React.StrictMode
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <HashRouter>
       <App />
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-// import tailwindcss from 'tailwindcss'
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   base: '/MyWebsite/',
   plugins: [react()],
   build: {
-    outDir: 'dist', 
+    sourcemap: true,
+    outDir: 'dist',
     emptyOutDir: true,
-    sourcemap: true
-  }
+  },
 });
